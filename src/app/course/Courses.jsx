d25aeb586593c86964b3e5e28a34b8110a0f1852
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-11-27 16:18:58 +0100

    feat: create dynamic routes

--- src/app/course/Courses.jsx
+++ src/app/course/Courses.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-
+import React from "react"
+import Link from "next/link"
 export default function Courses() {
-  return (
-    <nav>
-        <ul className='flex justify-between h-10 bg-blue-800 text-white text-2xl'>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>TailwindCSS</li>
-            <li>Javascript</li>
-            <li>React</li>
-        </ul>
-    </nav>
-  )
-}
+    return (
+        <nav className="flex justify-around h-10 bg-blue-800 text-white text-2xl">
+            <Link href = '/course'><li>All</li></Link>
+
+            <Link href = '/course/Fiction'><li>Fiction</li></Link>
+
+            <Link href = '/course/Non-Fiction'><li>Non-Fiction</li></Link>
+
+           <Link href = '/course/Self-Help'><li>Self-Help</li></Link> 
+
+           <Link href = '/course/Thriller'><li>Thriller</li></Link> 
+
+           <Link href = '/course/Memoir'><li>Memoir</li></Link> 
+
+           <Link href = '/course/Fantasy'><li>Fantasy</li></Link> 
+        </nav>
+    )
+    
+  }
--- src/app/course/page.jsx
+++ src/app/course/page.jsx
@@ -1,49 +1,117 @@
+import React from 'react'
+import Link from 'next/link'
 
-const books = [
+export const books = [
   {
-  id: '001',
-  title: 'Atomic Hsbits',
-  author: 'James Clear'
-},
-{
-  id: '002',
-  title: 'Things Fall Apart',
-  author: 'Chinua Achebe'
-},
-{
-  id: '003',
-  title: 'Harry Potter',
-  author: 'JK Rowling'
-},
-{
-  id: '004',
-  title: 'Summer Desserts',
-  author: 'Danielle Steele'
-},
-{
-  id: '005',
-  title: 'A Perfect Gentleman',
-  author:'Danielle Steele'
-},
-{
-  id: '006',
-  title: 'And Then I Met You',
-  author: 'Ella Dien'
-},
+    id: '001',
+    title: 'Atomic Habit',
+    author: 'James Clear',
+    year: 2018,
+    type: 'Self-Help',
+    summary: 'A guide to building good habits and breaking bad ones, backed by scientific research and real-life examples.'
+  },
+  {
+    id: '002',
+    title: 'Things Fall Apart',
+    author: 'Chinua Achebe',
+    year: 1958,
+    type: 'Fiction',
+    summary: 'A novel that explores the effects of British colonialism and Christian missionary efforts on the Igbo society in Nigeria.'
+  },
+  {
+    id: '003',
+    title: 'The Four Winds',
+    author: 'Kristin Hannah',
+    year: 2021,
+    type: 'Fiction',
+    summary: 'Set during the Great Depression, this novel follows a woman\'s journey from Texas to California in search of a better life.'
+  },
+  {
+    id: '004',
+    title: 'Klara and the Sun',
+    author: 'Kazuo Ishiguro',
+    year: 2021,
+    type: 'Fiction',
+    summary: 'A thought-provoking story about an Artificial Friend named Klara and her observations of human behavior.'
+  },
+  {
+    id: '005',
+    title: 'Project Hail Mary',
+    author: 'Andy Weir',
+    year: 2021,
+    type: 'Fiction',
+    summary: 'A gripping tale of a lone astronaut on a mission to save humanity, filled with humor, science, and unexpected twists.'
+  },
+  {
+    id: '006',
+    title: 'The Push',
+    author: 'Ashley Audrain',
+    year: 2021,
+    type: 'Thriller',
+    summary: 'A psychological novel that explores the complexities of motherhood and the impact of intergenerational trauma.'
+  },
+  // Additional entries with years, type, and summary
+  {
+    id: '007',
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+    year: 2019,
+    type: 'Thriller',
+    summary: 'A psychological mystery about a woman\'s sudden violence against her husband and her subsequent silence.'
+  },
+  {
+    id: '008',
+    title: 'Educated',
+    author: 'Tara Westover',
+    year: 2018,
+    type: 'Memoir',
+    summary: 'The memoir of a woman who grows up in a strict and abusive household but eventually escapes to pursue education.'
+  },
+  {
+    id: '009',
+    title: 'The Night Circus',
+    author: 'Erin Morgenstern',
+    year: 2011,
+    type: 'Fantasy',
+    summary: 'A magical tale of a mysterious circus that appears only at night, showcasing fantastical wonders and illusions.'
+  },
+  {
+    id: '010',
+    title: 'Sapiens: A Brief History of Humankind',
+    author: 'Yuval Noah Harari',
+    year: 2014,
+    type: 'Non-Fiction',
+    summary: 'A thought-provoking exploration of the history of Homo sapiens, from the emergence of our species to modern times.'
+  },
+  {
+    id: '011',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925,
+    type: 'Classic',
+    summary: 'A timeless classic that explores the American Dream and the decadence of the Jazz Age through the story of Jay Gatsby.'
+  },
+  {
+    id: '012',
+    title: 'Eleanor Oliphant Is Completely Fine',
+    author: 'Gail Honeyman',
+    year: 2017,
+    type: 'Fiction',
+    summary: 'A heartwarming and quirky novel about an eccentric woman\'s journey towards self-discovery and connection with others.'
+  }
 ]
 
-const bookCard = books.map(book => (
-  <section key={book.id} className="p-[4rem] shadow-2xl">
+const bookCard = books.map(book=>(
+  <section key={book.id} className='p-10 shadow-2xl'>
+    <Link href ={`/books/${book.title.split(' ').join('-')}`} title ={`${book.title} by ${book.author}`}>
     <h2>{book.title}</h2>
     <p>{book.author}</p>
+    </Link>
   </section>
 ))
 
-
 export default function Course() {
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-      {bookCard}
-    </div>
+    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>{bookCard}</div>
   )
 }
